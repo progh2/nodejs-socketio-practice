ae53237a5ada013f437a3600fd992b1b2049fa51
Gihun Ham <progh2@gmail.com>
2018-02-01 17:24:28 +0900

    #6 웹채팅 구현하기

--- index.js
+++ index.js
@@ -4,37 +4,24 @@ var fs = require('fs')
 var socketio = require('socket.io');
 
 // 웹 서버를 생성합니다.
-var server = http.createServer();
-// 소켓 서버를 생성 및 실행
-var io = socketio.listen(server);
-  var roomName = null;
-server.listen(3000, function(){
-  console.log('Server running at http://127.0.0.1:3000');
-});
-
-// 웹 서버 이벤트를 연결합니다.
-server.on('request', function (request, response){
+var server = http.createServer(function (request, response){
   // HTMULPage.html 파일을 읽습니다.
   fs.readFile('HTMLPage.html', function( error, data){
     response.writeHead(200, { 'Content-Type':'text/html'});
     response.end(data);
   });
+}).listen(3000, function(){
+  console.log('Server running at http://127.0.0.1:3000');
 });
 
+// 소켓 서버를 생성 및 실행
+var io = socketio.listen(server);
+
 io.sockets.on('connection', function(socket){
-  // 방 이름을 저장할 변수
-
-
-  // join 이벤트
-  socket.on('join', function(data){
-    console.log('방 조인! : ' + data);
-    roomName = data;
-    socket.join(data);
-  });
-
+  console.log('[컨넥션] 소켓 연결됨');
   // messasge 이벤트
   socket.on('message', function(data){
-    console.log(':::'+roomName + ' message event! : ' + data );
-    io.sockets.in(roomName).emit('message', data);
+    console.log('[메시지]' + data );
+    io.sockets.emit('message', data);
   });
 });
