6aa0284040b35617381d37cd15456e7c8a332b43
Gihun Ham <progh2@gmail.com>
2018-02-03 18:47:08 +0900

    #15 실시간 그림판

--- app.js
+++ app.js
@@ -2,50 +2,54 @@
 var socketio = require('socket.io');
 var express = require('express');
 var http = require('http');
+var ejs = require('ejs');
 var fs = require('fs');
-
-// 변수를 선언합니다.
-var seats = [
-    [1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-    [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
-];
 
 // 웹 서버를 생성합니다.
 var app = express();
+app.use(express.static('public'));
+
+// 웹 서버를 실행합니다.
 var server = http.createServer(app);
+server.listen(3000, function () {
+    console.log('server running at http://127.0.0.1:3000');
+});
 
 // 라우트를 수행합니다.
-app.get('/', function (request, response, next) {
-    fs.readFile('HTMLPage.html', function (error, data) {
+app.get('/', function (request, response) {
+    fs.readFile('lobby.html', function (error, data) {
         response.send(data.toString());
     });
 });
-app.get('/seats', function (request, response, next) {
-    response.send(seats);
+
+app.get('/canvas/:room', function (request, response) {
+    fs.readFile('canvas.html', 'utf8', function (error, data) {
+        response.send(ejs.render(data, {
+            room: request.params.room
+        }));
+    });
 });
 
-// 웹 서버를 실행합니다.
-server.listen(3000, function () {
-    console.log('Server Running at http://127.0.0.1:3000');
+app.get('/room', function (request, response) {
+    var rooms = Object.keys(io.sockets.adapter.rooms).filter(function (item) {
+        return item.indexOf('/') < 0;
+    })
+    response.send(rooms);
 });
 
-// 소켓 서버를 생성 및 실행합니다.
+// 소켓 서버를 생성합니다.
 var io = socketio.listen(server);
 io.sockets.on('connection', function (socket) {
-    socket.on('reserve', function (data) {
-        seats[data.y][data.x] = parseInt(data.user);
-        seats[data.y][data.x]['color'] = data.color;
-        console.log( "###############", data.user, "/", seats[data.y][data.x] );
-        io.sockets.emit('reserve', data);
+    var roomId = "";
+
+    socket.on('join', function (data) {
+        socket.join(data);
+        roomId = data;
+    });
+    socket.on('draw', function (data) {
+        io.sockets.in(roomId).emit('line', data);
+    });
+    socket.on('create_room', function (data) {
+        io.sockets.emit('create_room', data.toString());
     });
 });
